be8323c857d7656a6612d6a03202cb1c88a8ce3f
AmitaiB <blickstein@gmail.com>
2019-04-25 17:52:01 -0400

    Initial Commit

---
+++ TestRotationOverride_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRotationOverride_Swift
+//
+//  Created by Amitai Blickstein on 4/25/19.
+//  Copyright © 2019 Amitai Blickstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
